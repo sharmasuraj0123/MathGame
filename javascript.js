50d08e508cab2a4762c76a91b0fc4ab3bbfc9ec9
Suraj Sharma <sharmasuraj0123@gmail.com>
2018-01-17 17:08:53 -0500

    Fixed Indendation

--- javascript.js
+++ javascript.js
@@ -5,45 +5,29 @@ var action;
 var timeremaining;
 var correctAnswer;
 
-//if we click on the start/reset
 document.getElementById("startreset").onclick = function(){
     
-    //if we are playing
+
     
     if(playing == true){
         
         location.reload(); //reload page
         
-    }else{//if we are not playing
-        
-        //change mode to playing
-        
+    }else{
         playing = true;
-        
-        //set score to 0
-        
         score = 0;
         document.getElementById("scorevalue").innerHTML = score;
-     
-        //show countdown box 
-        
         show("timeremaining");
         timeremaining = 60;
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
         
-        //hide game over box
-        
         hide("gameOver");
         
-        //change button to reset
+       
         document.getElementById("startreset").innerHTML = "Reset Game";
         
-        //start countdown
         
         startCountdown();
-        
-        //generate a new Q&A
-        
         generateQA();
     }
     
@@ -82,18 +66,6 @@ for(i=1; i<5; i++){
     }
 }   
 }
-//if we click on answer box
-    //if we are playing
-        //correct?
-            //yes
-                //increase score
-                //show correct box for 1sec
-                //generate new Q&A
-            //no
-                //show try again box for 1sec
-
-
-//functions
 
 //start counter
 
@@ -114,13 +86,11 @@ function startCountdown(){
     }, 1000);    
 }
 
-//stop counter
 
 function stopCountdown(){
     clearInterval(action);   
 }
 
-//hide an element
 
 function hide(Id){
     document.getElementById(Id).style.display = "none";   
